24a0801b9f89ada9602e9479d313f97e28d46565
lupomontero <lupomontero@users.noreply.github.com>
2019-12-23 19:45:10 +0000

    Automated deployment: Mon Dec 23 19:45:10 UTC 2019 a0b0b73e9fd0d0efa7d3531518998deb5edba1df

---
+++ precache-manifest.9f1e3579375739430282df90a3be13eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8536aa7ce4d39dfe63ca25471328073b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87600a79bd79d6163904",
+    "url": "/static/css/main.fea0fbc2.chunk.css"
+  },
+  {
+    "revision": "365869923473b4c67261",
+    "url": "/static/js/2.f5e7db9f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f5e7db9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87600a79bd79d6163904",
+    "url": "/static/js/main.dbfcb5ee.chunk.js"
+  },
+  {
+    "revision": "1b975e514f956980183a",
+    "url": "/static/js/runtime-main.0eed31da.js"
+  }
+]);
